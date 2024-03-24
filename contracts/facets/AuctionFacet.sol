4f926f6f947ca72a2318cf4dc98b156bfd7a8a5f
Banwo Olorunsogo <sogobanwo@gmail.com>
2024-03-24 12:09:41 +0100

    feat: added Facets for auction to the diamond

---
+++ contracts/facets/AuctionFacet.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {IERC721} from "../interfaces/IERC721.sol";
+import {LibAppStorage} from "../libraries/LibAppStorage.sol";
+
+error ZERO_DURATION_NOT_ALLOWED();
+error ZERO_PRICE_NOT_ALLOWED();
+error NOT_OWNER_OF_NFT();
+error AUCTION_ID_NOT_FOUND();
+error BID_LESS_THAN_EXISTING_BID();
+error AUCTION_ENDED();
+error NOT_A_VALID_BID();
+error INSUFFICIENT_BALANCE();
+
+
+contract AuctionContract{
+
+    LibAppStorage.Layout internal l;
+
+    function createAuction(uint _durationInSeconds, uint _startingBid, uint _nftTokenId) external{
+
+        // checking user inputs
+        if (_durationInSeconds == 0) revert ZERO_DURATION_NOT_ALLOWED();
+
+        if (_startingBid == 0) revert ZERO_PRICE_NOT_ALLOWED();
+
+        // checking for owner of the NFT
+        if (IERC721(l.nftContractAddress).ownerOf(_nftTokenId) == msg.sender) revert NOT_OWNER_OF_NFT();
+
+        uint _newAuctionCount = l.auctionCount + 1;
+
+        LibAppStorage.AuctionDetails storage ad = l.auctions[_newAuctionCount];
+
+        // setting auction details
+        ad.auctionCreator = msg.sender;
+
+        ad.auctionId = _newAuctionCount;
+
+        ad.duration = _durationInSeconds;
+
+        ad.currentBid = _startingBid;
+
+        ad.nftTokenId = _nftTokenId;
+
+        ad.currentBid = _startingBid;
+
+        ad.minValidBid = 120 * ad.currentBid / 100;
+
+        ad.auctionCreatedTime = block.timestamp;
+
+        // transferring NFT to Contract
+        IERC721(l.nftContractAddress).safeTransferFrom(msg.sender, address(this), _nftTokenId);
+        
+        // incrementing auctionCount
+        l.auctionCount++;
+
+    }
+
+    function makeBid(uint _auctionId, uint _bid) external {
+
+        if(!l.auctions[_auctionId]) revert AUCTION_ID_NOT_FOUND();
+
+        LibAppStorage.AuctionDetails storage ad = l.auctions[_auctionId];
+
+        uint _auctionEllapseTime = ad.auctionCreatedTime + ad.duration;
+
+        if (block.timestamp > _auctionEllapseTime) revert AUCTION_ENDED();
+
+        if(_bid < ad.currentBid) revert BID_LESS_THAN_EXISTING_BID();
+
+        if(_bid < ad.minValidBid) revert NOT_A_VALID_BID();
+
+        uint256 _bidderBalance = l.balances[msg.sender];
+
+        if(_bidderBalance < _bid) revert INSUFFICIENT_BALANCE();
+
+        LibAppStorage._transferFrom(msg.sender, address(this), _bid);
+
+
+        if (ad.hightestBidder == address(0)){
+
+            ad.hightestBidder = msg.sender;
+
+        } else {
+
+            ad.previousBidder = ad.hightestBidder;
+
+            ad.hightestBidder = msg.sender;
+
+            ad.lastInteractor = msg.sender;
+
+            totalFeeDistribution(_bid, _auctionId);
+
+        }
+
+        ad.currentBid = _bid;
+
+
+    }
+
+    function totalFeeDistribution(uint _bid, uint _auctionId)  private {
+
+        LibAppStorage.AuctionDetails storage ad = l.auctions[_auctionId];
+
+        uint256 totalFee = (_bid * 10) / 100;
+
+        uint256 teamWalletFee = (totalFee * 20) / 100;
+
+        uint256 daoFee = (totalFee * 20) / 100;
+
+        uint256 burnedFee = (totalFee * 20) / 100;
+
+        uint256 previousBidderFee = (totalFee * 30) / 100;
+
+        uint256 lastInteractorFee = (totalFee * 10) / 100;
+
+        LibAppStorage._transferFrom(
+            address(this),
+            0xe902aC65D282829C7a0c42CAe165D3eE33482b9f,
+            teamWalletFee
+        );
+
+        LibAppStorage._transferFrom(
+            address(this),
+            0xe902aC65D282829C7a0c42CAe165D3eE33482b9f,
+            daoFee
+        );
+
+        LibAppStorage._transferFrom(
+            address(this),
+            address(0),
+            burnedFee
+        );
+
+        LibAppStorage._transferFrom(
+            address(this),
+            ad.previousBidder,
+            previousBidderFee
+        );
+
+        LibAppStorage._transferFrom(
+            address(this),
+            ad.hightestBidder,
+            lastInteractorFee
+        );
+        
+    }
+
+}
---
+++ contracts/libraries/LibAppStorage.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+error INSUFFICIENT_BALANCE();
+
+library LibAppStorage {
+	
+  struct AuctionDetails {
+
+        address auctionCreator;
+
+        address hightestBidder;
+
+        address previousBidder;
+
+        uint256 duration;
+
+        uint256 nftTokenId;
+
+        uint256 auctionId;
+
+        uint256 auctionCreatedTime;
+
+        uint256 currentBid;
+
+        uint256 minValidBid;
+
+        address lastInteractor;
+
+    }
+
+    struct Layout {
+
+        string name;
+
+        string symbol;
+
+        uint8 decimal;
+
+        address owner; 
+
+        uint256 totalSupply;
+
+        address nftContractAddress;
+
+        address auctionTokenFacetAddress;
+
+        uint auctionCount;
+
+        mapping(address => uint256) balances;
+
+        mapping(address => mapping(address => uint256)) allowances;
+
+        mapping (uint => AuctionDetails) auctions;
+
+    }
+
+    function layoutStorage() internal pure returns (Layout storage l) {
+        assembly {
+            l.slot := 0
+        }
+    }
+
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        Layout storage l = layoutStorage();
+        uint256 balance = l.balances[msg.sender];
+
+        if (balance < _amount) {
+            revert INSUFFICIENT_BALANCE();
+        }
+        l.balances[_from] = balance - _amount;
+        l.balances[_to] += _amount;
+    }
+
+
+}
